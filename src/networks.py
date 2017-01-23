696adcecdb03feb1401a9f4c43974e22d25ff1b1
justthedoctor <justthedoctor.931@gmail.com>
2017-01-23 16:26:56 -0600

    Code Update

---
+++ src/networks.py
@@ -0,0 +1,14 @@
+# Main network and testnet3 definitions
+
+params = {
+    'bitcoin_main': {
+        'pubkey_address': 51,
+        'script_address': 8,
+        'genesis_hash': 'dea6b9e435686c66f5178eb9f04e18b4fc040a639ccd5cd90f31e5343afb5b36'
+    },
+    'bitcoin_test': {
+        'pubkey_address': 111,
+        'script_address': 196,
+        'genesis_hash': '000000000933ea01ad0ee984209779baaec3ced90fa3f408719526f8d77f4943'
+    }
+}
